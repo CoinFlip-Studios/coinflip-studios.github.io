68818334f2889ac9c6adc1c6cab93665bc4b8228
ChrisMadXR <71994685+iemchriis@users.noreply.github.com>
2026-08-05 18:25:58 +0800

    fix

--- home/webAR/ARRacingV8/ServiceWorker.js
+++ home/webAR/ARRacingV8/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-ARBasketball-0.1.0";
 const contentToCache = [
-    "Build/ARRacingV7.loader.js",
-    "Build/ARRacingV7.framework.js",
-    "Build/ARRacingV7.data",
-    "Build/ARRacingV7.wasm",
+    "Build/ARRacingV8.loader.js",
+    "Build/ARRacingV8.framework.js",
+    "Build/ARRacingV8.data",
+    "Build/ARRacingV8.wasm",
     "TemplateData/style.css"
 
 ];
